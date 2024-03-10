e666b309d71efaa300ca98cd72a98fdfce398f21
okarslan <okarslan@dabnet.com.tr>
2024-03-10 14:20:25 +0300

    position

--- src/screens/DesignScreen.js
+++ src/screens/DesignScreen.js
@@ -3,24 +3,16 @@ import React from "react";
 
 export default function DesignScreen() {
   return (
-    <View style={styles.mainView}>
-      <Text style={styles.mainText}>Layout</Text>
+    <View style={{width:'100%',height:'100%'}}>
+        <View style ={{width:'25%', height:'25%', backgroundColor:'red'}}/>
+        <View style ={{width:'25%', height:'25%', backgroundColor:'blue',position:'absolute',right:0}}/>
+        <View style ={{width:'25%', height:'25%', backgroundColor:'brown',position:'absolute',bottom:0}}/>
+        <View style ={{width:'25%', height:'25%', backgroundColor:'green',position:'absolute',right:0,bottom:0}}/>
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    mainView:{
-        borderWidth:1,
-        borderColor:'black',
-        margin:20,
-        borderRadius:20,
-    },
-    mainText:{
-        borderWidth:1,
-        borderColor:'red',
-        margin:30,
-        padding:20,
-        borderRadius:10
-    }
+   
 });
